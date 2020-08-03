8a424c6ed65ac6fc7a092647f4a203bcd2a3692b
shubham <shubham5010175@gmail.com>
2020-08-03 16:33:10 +0530

    AzureBooksApp
    - Calls a free Azure API
    - Reads a list of books from it as JSON data
    - Parses JSON
    -Shows books in tableView
    -On clicking a cell, shows the details of book in another page.
    
    TBD : Introduce codable protocol

---
+++ AzureBooks/AzureBooks/Book.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Book{
+    var id = 0
+    var title = ""
+    var description = ""
+    var pageCount = 0
+    var excerpt = ""
+    var publishDate = "2020-07-31T09:35:32.233Z"
+}
---
+++ AzureBooks/AzureBooks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AzureBooks
+//
+//  Created by Shubham Vaishnav on 31/07/20.
+//  Copyright © 2020 Capgemini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
